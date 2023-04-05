661e9000748d350df5afe38f3d31db47651ad9cd
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-04-04 21:24:14 -0400

    mongoose update

---
+++ views/places/new.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+const Def = require("../default");
+
+<div className="form-group">
+  <label for="founded">Founded Year</label>
+  <input className="form-control" id="founded" name="founded" />
+</div>;
+
+module.exports = new_form;
