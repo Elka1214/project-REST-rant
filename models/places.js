bf9cbefb58361d5ad51c5a03080450e091ba8339
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-04-07 21:50:35 -0400

    helper method added

--- models/places.js
+++ models/places.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: "http://placekitten.com/350/350" },
   cuisines: { type: String, required: true },
   city: { type: String, default: "Any town" },
   state: { type: String, default: "USA" },
@@ -16,4 +16,5 @@ const placeSchema = new mongoose.Schema({
 placeSchema.methods.showEstablished = function () {
   return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`;
 };
+
 module.exports = mongoose.model("Place", placeSchema);
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -17,6 +17,16 @@ function new_form() {
               value={new Date().getFullYear}
             />
           </div>
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded Year</label>
+            <input
+              type="number"
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={new Date().getFullYear()}
+            />
+          </div>
 
           <div className="col-sm-6 col-md-4 col-lg-3">
             <label htmlFor="name">Place Name</label>
